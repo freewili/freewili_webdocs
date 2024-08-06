d2402f738824914036fbc7a0d74a3f9e45b393ce
pbawane-freewili <pbawane@freewili.com>
2024-08-06 17:17:06 +0530

    set trailingSlash: true and enabled it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
     locales: ['en'],
   },
 
-  // trailingSlash: false, // Set global trailingSlash configuration 
+  trailingSlash: true, // Set global trailingSlash configuration 
 
   presets: [
     [
